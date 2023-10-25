bf85e439df37a09ca0dd6e1c704539378374a3f3
MeinChef <yapa.nilop@gmail.com>
2023-10-25 15:10:45 +0200

    tried stuff, didn't work

--- other_stuff.py
+++ other_stuff.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def meows(n):
     calls = 0
     while calls < n:
@@ -17,3 +19,10 @@ if __name__ == "__main__":
 
     for i in meows(10):
         print(i)
+
+    # task 4
+    arr = np.random.normal(0,1,25).reshape((5,5))
+    arr[arr > 0.09] = arr * arr
+    arr[arr < 0.09] = 42
+
+    print(arr)
