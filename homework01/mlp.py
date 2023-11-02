ae63c54b81edb51478255ffd730918f7d2f618d5
Mini11e <dmuth@uos.de>
2023-11-02 12:49:02 +0100

    some failed ideas

--- homework01/mlp.py
+++ homework01/mlp.py
@@ -7,12 +7,18 @@ def sigmoid(x):
     sig = 1/(1 + np.exp(-x))
     return sig
 
+#load data
+#return_X_y=True makes it into a tuple of two array
+#as_frame=True makes it into two dataframes
+#no idea hpw to convert one tuple of two arrays(datas)(targets) into individual tuples of (data, target) 
+data = load_digits(n_class=10, return_X_y=True, as_frame=True)
 
-digits = load_digits(n_class=10, return_X_y=True, as_frame=True)
+#This below works with normal arrays, but ours are stupid:
+#ValueError: setting an array element with a sequence. The requested array has an inhomogeneous 
+#shape after 2 dimensions. The detected shape was (2, 1797) + inhomogeneous part
 
+#x = data[0]
+#y = data[1]
+#newdata = np.array((x,y))
 
-#plt.gray()
-#plt.matshow(digits.images[0])
-#plt.show()
-
-print(digits)
+#print(newdata)
