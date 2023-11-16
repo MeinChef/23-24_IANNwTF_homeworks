3dac8ea1ba0e40637e0dcdfb61560d719c06ce3b
MeinChef <yapa.nilop@gmail.com>
2023-11-16 18:59:43 +0100

    added comparison models

--- homework02/func.py
+++ homework02/func.py
@@ -1,24 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-
-def visualise(acc_train, acc_epoch):
-    fig, (ax1, ax2) = plt.subplots(nrows = 1, ncols = 2)
-    
-    ax1.plot(acc_epoch)
-    ax1.set_xlabel('epochs')
-    ax1.set_ylabel('accuracy')
-    
-    ax2.plot(acc_train)
-    ax2.set_xlabel('every 20th image')
-    ax2.set_ylabel('accuracy')
-    
-    ax1.set_ylim(0,1)
-    ax1.sharey(ax2)
-
-    fig.tight_layout()
-
-    plt.show()
+import mlp
 
 
 
@@ -57,17 +40,51 @@ def visualise(acc_epoch, acc_train, loss_epoch, loss_train):
 
     plt.show()
 
+def vis_accs(accs):
+
+    cols = ['Base', 'Change 1', 'Change 2']
+    rows = ['Learning Rate', 'Batchsize', 'No. Layers', 'No. Neurons/Layer', 'Optimiser']
+
+    titles = [[0.1, 0.03, 0.5], [128, 32, 512], [2, 1, 8], [256, 64, 512], ['SGD', 'Lion', 'Adam']]
+    fig, axes = plt.subplots(nrows = 5, ncols = 3)
+    fig.tight_layout()  
+
+    for i in range(5):
+        for j in range(3):
+            axes[i][j].plot(accs[i][j])
+            axes[i][j].axhline(1, alpha = 0.5, color = "red", linestyle = "--")
+            axes[i][j].set_ylim(0, 1.2)
+            axes[i][j].set_xlabel('Epochs')
+            axes[i][j].set_ylabel('Accuracy')
+            axes[i][j].set_title(titles[i][j])
+
+    for ax, col in zip(axes[0], cols):
+        ax.annotate(col, xy = (0.5, 1.2), xytext=(0, 5),
+                xycoords = 'axes fraction', textcoords = 'offset points',
+                size = 'large', ha = 'center', va = 'baseline')
+
+    for ax, row in zip(axes[:,0], rows):
+        ax.annotate(row, xy = (0, 0.5), xytext = (- ax.yaxis.labelpad - 5, 0),
+                xycoords = ax.yaxis.label, textcoords = 'offset points',
+                size = 'large', ha = 'right', va = 'center')
+    
+    
+    fig.set_size_inches(10, 30)
+    
+    plt.show()
 
 
-def pipeline(ds):
+
+def pipeline(ds, batch_size = 128):
     ds = ds.map(lambda image, label: ((tf.cast(image, tf.float32)/128)-1, label))
     ds = ds.map(lambda image, label: (tf.reshape(image, (-1,)), label))
     ds = ds.map(lambda image, label: (image, tf.one_hot(label, depth = 10)))
-    ds = ds.batch(128)
+    ds = ds.batch(batch_size)
     ds = ds.prefetch(16)
     return ds
 
-def training(model, 
+
+def training_track_all_params(model, 
              train,
              test,
              optimiser,
@@ -99,28 +116,37 @@ def training(model,
             counter += 1
 
         acc_test[epoch], loss_test[epoch] = testing(model, test, loss_func)
-        
-        
-        # maybe we nedd to not pass it over to a function 
-        #
-        # accuracy = np.zeros(len(test))
-        # i = 0
-        # 
-        # for x, target in test:
-        #     pred = model(x)
-        #     pred = tf.nn.softmax(pred)
-        #     
-        #     accuracy[i] = np.mean(np.argmax(pred, -1) == np.argmax(target, -1))
-        #     #print(accuracy[i])
-        #     i += 1
-        # 
-        # acc_test[epoch] = np.mean(accuracy)
-
 
         print(f'Epoch {epoch}: with an accuracy of {round(acc_test[epoch], ndigits = 4)} and loss of {round(loss_test[epoch], ndigits = 4)}')
 
            
     return np.mean(acc_self, axis = 0), acc_test, np.mean(loss_self, axis = 0), loss_test
+
+def training_track_acc_epoch(model, 
+             train,
+             test,
+             optimiser,
+             loss_func = tf.keras.losses.CategoricalCrossentropy(from_logits = True),
+             epochs = 2):
+    
+    acc_test = np.empty(epochs)
+    
+    for epoch in range(epochs):
+        for x, target in train:
+            
+            with tf.GradientTape() as tape: #track accuracy within training
+                pred = model(x)
+                loss = loss_func(target, pred)
+
+            gradients = tape.gradient(loss, model.variables) #calculate outside of the GradientTape context
+            optimiser.apply_gradients(zip(gradients, model.variables)) 
+
+        acc_test[epoch], _ = testing(model, test, loss_func)
+
+        print(f'Epoch {epoch}: with an accuracy of {round(acc_test[epoch], ndigits = 4)}')
+
+           
+    return acc_test,
 
 
 def testing(model, test, loss_func):
@@ -137,5 +163,47 @@ def testing(model, test, loss_func):
         accuracy[i] = np.mean(np.argmax(pred, -1) == np.argmax(target, -1))
 
         i += 1
+        
 
-    return np.mean(accuracy), np.mean(loss)
+    return np.mean(accuracy), np.mean(loss) - 1
+
+
+
+########################################################################################
+# Variations of the ANN
+########################################################################################
+
+def var_learn(learning_rate, train, test, ann):
+
+    optimiser = tf.keras.optimizers.legacy.SGD(learning_rate = learning_rate)
+
+    return training_track_acc_epoch(ann, train, test, optimiser)
+
+
+
+def var_batch(batch, train_ds, test_ds, ann):
+
+    train = pipeline(train_ds, batch)
+    test = pipeline(test_ds, batch)
+
+    optimiser = tf.keras.optimizers.legacy.SGD(learning_rate = 0.1)
+    return training_track_acc_epoch(ann, train, test, optimiser)
+
+
+def var_net(layer, train, test, optimiser):
+
+    ann = mlp.MLP_Model([128] * layer)
+    return training_track_acc_epoch(ann, train, test, optimiser)
+
+
+def var_layer(neurons, train, test, optimiser):
+
+    ann = mlp.MLP_Model([neurons] * 2)
+    return training_track_acc_epoch(ann, train, test, optimiser)
+
+
+def var_opt(opt, ann, train, test):
+
+    return training_track_acc_epoch(ann, train, test, opt)
+    
+
--- homework02/main.py
+++ homework02/main.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 import mlp 
 import func
+import numpy as np
 
 
 if __name__ == "__main__":
@@ -10,18 +11,70 @@ if __name__ == "__main__":
     (train_ds, test_ds), ds_info = tfds.load ('mnist' , split =['train', 'test'], as_supervised = True, with_info = True, shuffle_files = True)
     
     # print(ds_info)
+    #
     # How many training/test images are there?: training(60000), test(10000)
     # What's the image shape?: (28, 28, 1) -> (pixel, pixel, white value)
     # What range are pixel values in?: 0-255
     # tfds.show_examples (train_ds , ds_info)
-
+    
     train = func.pipeline(train_ds)
     test = func.pipeline(test_ds)
     
     ann = mlp.MLP_Model(layer_sizes = [256, 256]) # task 3: more neurons or more layers?
     optimiser = tf.keras.optimizers.legacy.SGD(learning_rate = 0.1)
+    
+    
+    acc_self, acc_test, loss_self, loss_test = func.training_track_all_params(ann, train, test, optimiser)
+
+    #func.visualise(acc_test, acc_self, loss_test, loss_self)
+    
 
 
-    acc_self, acc_test, loss_self, loss_test = func.training(ann, train, test, optimiser, epochs = 10)
+    ###################################
+    # Altering Parameters:
+    ###################################
+    # learning rate                 base, 2 params: (0.1, 0.03, 0.5)
+    # batchsize                     base, 2 params: (128, 32, 512)
+    # no. o. layers                 base, 2 params: (2, 1, 8)
+    # no. o. neurons. per layer     base, 2 params: (256, 64, 512)
+    # optimiser                     base, 2 params: (sgd, lion, adam)
 
-    func.visualise(acc_test, acc_self, loss_test, loss_self)
+
+    learning_rates = np.array([0.03, 0.5])
+    batch_sizes = np.array([32, 512])
+    network_sizes = np.array([1, 8])
+    layer_sizes = np.array([64, 512])   
+    optimisers = [tf.keras.optimizers.Lion(), tf.keras.optimizers.Adam()] 
+
+
+    acc_learn = [acc_test]
+    acc_batch = [acc_test]
+    acc_net = [acc_test]
+    acc_layer = [acc_test]
+    acc_opt = [acc_test]
+    
+    ann0 = mlp.MLP_Model(layer_sizes = [256, 256])
+
+    for learning_rate, i in zip(learning_rates, range(2)): 
+        acc_learn.append(func.var_learn(learning_rate, train, test, ann0))
+
+
+    ann0 = mlp.MLP_Model(layer_sizes = [256, 256])
+    
+    for batch, i in zip(batch_sizes, range(2)): 
+        acc_batch.append(func.var_batch(batch, train_ds, test_ds, ann0))
+
+    for network_size, i in zip(network_sizes, range(2)): 
+        acc_net.append(func.var_net(network_size, train, test, optimiser))
+    
+    for layer, i in zip(layer_sizes, range(2)):
+        acc_layer.append(func.var_layer(layer, train, test, optimiser))
+
+    for opt, i in zip(optimisers, range(2)):
+        acc_opt.append(func.var_opt(opt, ann, train, test))
+
+    accuracy_array = np.array([acc_learn, acc_batch, acc_net, acc_layer, acc_opt])
+
+    print(accuracy_array.shape)
+    func.vis_accs(accuracy_array)
+
